dbf2b4fae9cdbb3a5f03d0593bad3d1d7a5acc0b
Nicolás Hertzulis <nicohertzulis@gmail.com>
2017-09-17 03:01:05 -0300

    [solver.asm] Esqueleto de las funciones

---
+++ solver.asm
@@ -0,0 +1,28 @@
+section .text
+
+global solver_lin_solve
+solver_lin_solve:
+; void solver_lin_solve ( fluid_solver* solver, uint32_t b, float * x, float * x0, float a, float c )
+; rdi = fluid_solver* solver
+; esi = uint32_t b
+; rdx = float* x
+; rcx = float* x0
+; xmm0 = float a
+; xmm1 = float b
+ret
+
+global solver_set_bnd
+solver_set_bnd:
+; void solver_set_bnd ( fluid_solver* solver, uint32_t b, float * x )
+; rdi = fluid_solver* solver
+; esi = uint32_t b
+; rdx = float* x
+ret
+
+global solver_project
+solver_project:
+; void solver_project ( fluid_solver* solver, float * p, float * div )
+; rdi = fluid_solver* solver
+; rsi = float* p
+; rdx = float* div
+ret
