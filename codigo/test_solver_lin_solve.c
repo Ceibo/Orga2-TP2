05f852ddfdeb870694c871570f6e7f4e2697c6fe
Nicolás Hertzulis <nicohertzulis@gmail.com>
2017-09-30 14:43:52 -0300

    [Test solver_lin_solve] Aprobado con precisión 10^-6

--- codigo/test_solver_lin_solve.c
+++ codigo/test_solver_lin_solve.c
@@ -1,6 +1,67 @@
 #include "solver.h"
 #include "assert.h"
 
+// Constantes
+const float a = 0.3f;
+const float c = 2.8f;
+const long double diferencia_maxima_permitida_en_comparaciones = 0.000001l;
+
+void test_solver_lin_solve(uint32_t size, uint32_t b) {
+  // Configuración inicial
+  fluid_solver* solver = solver_create(size, 0.05, 0, 0);
+  solver_set_initial_velocity(solver);
+  solver_set_bnd(solver, b, solver->u);
+  solver_set_bnd(solver, b, solver->v);
+
+  // Copias de las matrices para testear
+  size += 2;
+  float* u = (float*) malloc(sizeof(float) * size * size);
+  float* v = (float*) malloc(sizeof(float) * size * size);
+  uint32_t i, j;
+  for (i = 0; i < size; ++i) {
+    for (j = 0; j < size; ++j) {
+      u[IX(j, i)] = solver->u[IX(j, i)];
+      v[IX(j, i)] = solver->v[IX(j, i)];
+    }
+  }
+
+  // Pruebas
+  solver_lin_solve_c(solver, b, solver->u, solver->v, a, c);
+  solver_lin_solve(solver, b, u, v, a, c);
+  for (i = 0; i < size; ++i) {
+    for (j = 0; j < size; ++j) {
+      assert(fabs(u[IX(i, j)] - solver->u[IX(i, j)]) < diferencia_maxima_permitida_en_comparaciones);
+      assert(fabs(v[IX(i, j)] - solver->v[IX(i, j)]) < diferencia_maxima_permitida_en_comparaciones);
+    }
+  }
+
+  // Limpieza
+  solver_destroy(solver);
+  free(u);
+  free(v);
+}
+
 int main() {
+  test_solver_lin_solve(4, 0);
+  test_solver_lin_solve(4, 1);
+  test_solver_lin_solve(4, 2);
+  test_solver_lin_solve(16, 0);
+  test_solver_lin_solve(16, 1);
+  test_solver_lin_solve(16, 2);
+  test_solver_lin_solve(32, 0);
+  test_solver_lin_solve(32, 1);
+  test_solver_lin_solve(32, 2);
+  test_solver_lin_solve(64, 0);
+  test_solver_lin_solve(64, 1);
+  test_solver_lin_solve(64, 2);
+  test_solver_lin_solve(128, 0);
+  test_solver_lin_solve(128, 1);
+  test_solver_lin_solve(128, 2);
+  test_solver_lin_solve(256, 0);
+  test_solver_lin_solve(256, 1);
+  test_solver_lin_solve(256, 2);
+  test_solver_lin_solve(512, 0);
+  test_solver_lin_solve(512, 1);
+  test_solver_lin_solve(512, 2);
   return 0;
 }
