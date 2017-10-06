33e82d550dba8ea0828352bbf49e49a035bf2325
@rgramos <riki_german@yahoo.com.ar>
2017-10-06 12:05:12 -0300

    código para generar los csv que usa jupyter

--- codigo/resultados.c
+++ codigo/resultados.c
@@ -8,17 +8,14 @@
 
 #define IXX(i,j) ((i)+(size+2)*(j))
 
-char* archivoResultados  =  "resultados.csv";
+char* archivoResultados  =  "resultados.txt";
 uint32_t funcionATestear;
 uint32_t repeticiones = 100;
-uint32_t tamano = 512;
-char* codigoATestear = "asm";
+uint32_t tamano = 256; //16, 32, 64, 128, 256, 512
 
-// [ATENCIÓN]
-// Ir probando distintos valores
-// (estos valores afectan a solver_lin_solve)
-const float a = 1.0f; // 0.3f
-const float c = 4.0f; // 2.8f
+//parAmetros solver lin solve , opciones
+const float a = 1.0f;        //1.0f, 0.3f , -10.0f
+const float c = 4.0f;        //4.0f, 2.8f , 0.02f
 
 //char *c;
 
@@ -29,144 +26,220 @@ void flush_cache() {
         	c[j] = i*j;
 }*/
 
-unsigned long solver_set_bnd_ticks_count(uint32_t size, uint32_t b) {
+unsigned long solver_set_bnd_ticks_count_asm(uint32_t size, uint32_t b) {
 	fluid_solver* solver = solver_create(size, 0.05, 0, 0);
 	solver_set_initial_velocity(solver);
-
+	solver_set_initial_density(solver);
 	unsigned long start, end;
-  if (strcmp(codigoATestear, "asm") == 0) {
-  	RDTSC_START(start);
-  	solver_set_bnd(solver, b, solver->v);
-  	RDTSC_STOP(end);
-  } else {
-    RDTSC_START(start);
-    solver_set_bnd_c(solver, b, solver->v);
-    RDTSC_STOP(end);
-  }
+	RDTSC_START(start);
+	
+	solver_set_bnd(solver, b, solver->v);// versiOn asm 
 
+	RDTSC_STOP(end);
 	solver_destroy(solver);
 	return end - start;
 }
 
-void test_solver_set_bnd() {
-	size_t i;
-	unsigned long* ticks = (unsigned long*) malloc(sizeof(unsigned long) * repeticiones * 3);
-	//c = (char *)malloc(flush_cache_size);
-	for (i = 0; i < repeticiones; ++i) {
-		//flush_cache();
-		ticks[i] = solver_set_bnd_ticks_count(tamano, 1);
-	}
-	for (i = 0; i < repeticiones; ++i) {
-		//flush_cache();
-		ticks[i + repeticiones] = solver_set_bnd_ticks_count(tamano, 2);
-	}
-	for (i = 0; i < repeticiones; ++i) {
-		//flush_cache();
-		ticks[i + 2*repeticiones] = solver_set_bnd_ticks_count(tamano, 3);
-	}
-	printf("%lu", ticks[0]);
-	for (i = 1; i < repeticiones*3; i++) {
-		printf(",%lu", ticks[i]);	
-	}
-	free(ticks);
-	//free(c);
+
+unsigned long solver_set_bnd_ticks_count_c(uint32_t size, uint32_t b) {
+	fluid_solver* solver = solver_create(size, 0.05, 0, 0);
+	solver_set_initial_velocity(solver);
+    solver_set_initial_density(solver);
+	unsigned long start, end;
+	RDTSC_START(start);
+	
+	solver_set_bnd_c(solver, b, solver->v);// versiOn c
+
+	RDTSC_STOP(end);
+	solver_destroy(solver);
+	return end - start;
 }
 
-unsigned long solver_project_ticks_count(uint32_t size, uint32_t b) {
+unsigned long solver_lin_solve_ticks_count_c(uint32_t size, uint32_t b, float a, float c) {
   fluid_solver* solver = solver_create(size, 0.05, 0, 0);
   solver_set_initial_velocity(solver);
-  solver_set_bnd(solver, b, solver->u);
-  solver_set_bnd(solver, b, solver->v);
+  solver_set_initial_density(solver);
+  unsigned long start, end;
+  
+    RDTSC_START(start);
+    solver_lin_solve_c(solver, b, solver->u, solver->v, a, c); //versiOn c
+    RDTSC_STOP(end);
+  
+
+  solver_destroy(solver);
+  return end - start;
+}
+
+unsigned long solver_lin_solve_ticks_count_asm(uint32_t size, uint32_t b, float a, float c) {
+  fluid_solver* solver = solver_create(size, 0.05, 0, 0);
+  solver_set_initial_velocity(solver);
+  solver_set_initial_density(solver);
+
+  unsigned long start, end;
+  
+    RDTSC_START(start);
+    solver_lin_solve(solver, b, solver->u, solver->v, a, c); //versiOn c
+    RDTSC_STOP(end);
+  
+
+  solver_destroy(solver);
+  return end - start;
+}
+
+      //*****************************************************************************//
+
+unsigned long solver_project_ticks_count_c(uint32_t size) {
+  fluid_solver* solver = solver_create(size, 0.05, 0, 0);
+  solver_set_initial_velocity(solver);
+    solver_set_initial_density(solver);
 
   float* div = (float*) malloc(sizeof(float) * (size+2) * (size+2));
   float* p = (float*) malloc(sizeof(float) * (size+2) * (size+2));
 
-  uint32_t i, j, k;
+  uint32_t i, j;
+  float k,h;
 
   // [ATENCIÓN]
-  // Ir probando con distintos valores de llenado de la matriz
-  k = 0;
+  // Ir probando con distintos valores de llenado de la matriz, 
+  //                                         opciones matriz
+  k =0.1; h = 0.2  ; //k = 0.1,h =0.2 ; k = 0.09, h = -100 ; k = -10, h = 0.08 ; k = 1000, h = 2000
   for (i = 0; i < (size+2); ++i) {
     for (j = 0; j < (size+2); ++j) {
       div[IXX(j, i)] = k;
-      p[IXX(i, j)] = k;
+      p[IXX(i, j)] = h;
       k++;
+      h++;
     }
   }
-
+/* */
   unsigned long start, end;
-  if (strcmp(codigoATestear, "asm") == 0) {
-    RDTSC_START(start);
-    solver_project(solver, p, div);
-    RDTSC_STOP(end);
-  } else {
+  
     RDTSC_START(start);
     solver_project_c(solver, p, div);
     RDTSC_STOP(end);
-  }
+  
 
   solver_destroy(solver);
   return end - start;
 }
+
+      //************************************************************************************//
+
+unsigned long solver_project_ticks_count_asm(uint32_t size) {
+  fluid_solver* solver = solver_create(size, 0.05, 0, 0);
+  solver_set_initial_velocity(solver);
+    solver_set_initial_density(solver);
+
+  float* div = (float*) malloc(sizeof(float) * (size+2) * (size+2));
+  float* p = (float*) malloc(sizeof(float) * (size+2) * (size+2));
+
+  uint32_t i, j;
+  float k,h;
+  // [ATENCIÓN]
+  // Ir probando con distintos valores de llenado de la matriz, 
+  //                                         opciones matriz
+  k =  0.1; h = 0.2;//k = 0.1,h =0.2 ; k = 0.09, h = -100 ; k = -10, h = 0.08 ; k = 1000, h = 2000
+  for (i = 0; i < (size+2); ++i) {
+    for (j = 0; j < (size+2); ++j) {
+      div[IXX(j, i)] = k;
+      p[IXX(i, j)] = h;
+      k++;
+      h++;
+    }
+  }
+/* */
+  unsigned long start, end;
+  
+    RDTSC_START(start);
+    solver_project(solver, p, div);
+    RDTSC_STOP(end);
+  
+
+  solver_destroy(solver);
+  return end - start;
+}
+
+//***************************************************************************************//
+//****************** por cada test genera 2 tiradas: 100 para c y 100 asm *****************//
+//****************************************************************************************//
 
 void test_solver_project() {
   size_t i;
-  unsigned long* ticks = (unsigned long*) malloc(sizeof(unsigned long) * repeticiones * 3);
+  unsigned long* ticks_asm = (unsigned long*) malloc(sizeof(unsigned long) * repeticiones );
+  	unsigned long* ticks_c = (unsigned long*) malloc(sizeof(unsigned long) * repeticiones );
+
   for (i = 0; i < repeticiones; ++i) {
-    ticks[i] = solver_project_ticks_count(tamano, 1);
-  }
-  for (i = 0; i < repeticiones; ++i) {
-    ticks[i + repeticiones] = solver_project_ticks_count(tamano, 2);
-  }
-  for (i = 0; i < repeticiones; ++i) {
-    ticks[i + 2*repeticiones] = solver_project_ticks_count(tamano, 3);
-  }
-  printf("%lu", ticks[0]);
-  for (i = 1; i < repeticiones*3; i++) {
-    printf(",%lu", ticks[i]); 
-  }
-  free(ticks);
+		//flush_cache();
+		ticks_c[i] = solver_project_ticks_count_c(tamano);
+	}
+	for (i = 0; i < repeticiones; ++i) {
+		//flush_cache();
+		ticks_asm[i] = solver_project_ticks_count_asm(tamano);
+	}
+  
+  //printeamos , ticks_c = resultados c, ticks_asm = resultados asm , cantidad resultados = 100
+	printf("%s%s%s\n", "Repeticion,","c,","asm"); //agregar etiqueta : c, asm
+	for (i = 0; i < repeticiones; i++) {
+		printf("%ld",i+1);
+		printf(",%lu,", ticks_c[i]);	
+		printf("%lu\n", ticks_asm[i]);	
+	}
+	free(ticks_c);
+    free(ticks_asm);
 }
 
-unsigned long solver_lin_solve_ticks_count(uint32_t size, uint32_t b, float a, float c) {
-  fluid_solver* solver = solver_create(size, 0.05, 0, 0);
-  solver_set_initial_velocity(solver);
-  solver_set_bnd(solver, b, solver->u);
-  solver_set_bnd(solver, b, solver->v);
+void test_solver_set_bnd() {
+	size_t i;
+	unsigned long* ticks_asm = (unsigned long*) malloc(sizeof(unsigned long) * repeticiones );
+	unsigned long* ticks_c = (unsigned long*) malloc(sizeof(unsigned long) * repeticiones );
 
-  unsigned long start, end;
-  if (strcmp(codigoATestear, "asm") == 0) {
-    RDTSC_START(start);
-    solver_lin_solve(solver, b, solver->u, solver->v, a, c);
-    RDTSC_STOP(end);
-  } else {
-    RDTSC_START(start);
-    solver_lin_solve_c(solver, b, solver->u, solver->v, a, c);
-    RDTSC_STOP(end);
-  }
-
-  solver_destroy(solver);
-  return end - start;
+	//c = (char *)malloc(flush_cache_size);
+	for (i = 0; i < repeticiones; ++i) {
+		//flush_cache();
+		ticks_c[i] = solver_set_bnd_ticks_count_c(tamano, 2);
+	}
+	for (i = 0; i < repeticiones; ++i) {
+		//flush_cache();
+		ticks_asm[i] = solver_set_bnd_ticks_count_asm(tamano, 2);
+	}
+	
+	//printeamos , ticks_c = resultados c, ticks_asm = resultados asm , cantidad resultados = 100
+	printf("%s%s%s\n", "N repeticion,","c,","asm"); //agregar etiqueta : c, asm
+	for (i = 0; i < repeticiones; i++) {
+		printf("%ld",i+1);
+		printf(",%lu,", ticks_c[i]);	
+		printf("%lu\n", ticks_asm[i]);	
+	}
+	free(ticks_c);
+    free(ticks_asm);
+	//free(c);
 }
 
 void test_solver_lin_solve() {
   size_t i;
-  unsigned long* ticks = (unsigned long*) malloc(sizeof(unsigned long) * repeticiones * 3);
+  unsigned long* ticks_c = (unsigned long*) malloc(sizeof(unsigned long) * repeticiones );
+  unsigned long* ticks_asm = (unsigned long*) malloc(sizeof(unsigned long) * repeticiones );
+
   for (i = 0; i < repeticiones; ++i) {
-    ticks[i] = solver_lin_solve_ticks_count(tamano, 1, a, c);
+    ticks_c[i] = solver_lin_solve_ticks_count_c(tamano, 2, a, c);// 2do parAmetro: 1,2,3,10
   }
   for (i = 0; i < repeticiones; ++i) {
-    ticks[i + repeticiones] = solver_lin_solve_ticks_count(tamano, 2, a, c);
+    ticks_asm[i] = solver_lin_solve_ticks_count_asm(tamano, 2, a, c);//2do parAmetro: 1,2,3,10
   }
-  for (i = 0; i < repeticiones; ++i) {
-    ticks[i + 2*repeticiones] = solver_lin_solve_ticks_count(tamano, 3, a, c);
-  }
-  printf("%lu", ticks[0]);
-  for (i = 1; i < repeticiones*3; i++) {
-    printf(",%lu", ticks[i]); 
-  }
-  free(ticks);
+  
+  //printeamos , ticks_c = resultados c, ticks_asm = resultados asm , cantidad resultados = 100
+	printf("%s%s%s\n", "Repeticion,","c,","asm"); //agregar etiqueta : c, asm
+	for (i = 0; i < repeticiones; i++) {
+		printf("%ld",i+1);
+		printf(",%lu,", ticks_c[i]);	
+		printf("%lu\n", ticks_asm[i]);	
+	}
+  free(ticks_c);
+  free(ticks_asm);
 }
+
+//void test_solver_project() {}
+//void test_solver_lin_solve() {}
 
 void run_tests() {
 	switch (funcionATestear){
@@ -192,7 +265,7 @@ int main (int argc, char ** argv) {
 	} else if (strcmp(argv[1], "solver_project") == 0) {
 		funcionATestear = 2;
 	} else {
-		printf("Nombre de función desconocido. Las opciones son: solver_lin_solve, solver_set_bnd, solver_project\n");
+		printf("Nombre de función desconocido, las opciones son: solver_lin_solve, solver_set_bnd, solver_project\n");
 		exit(EXIT_FAILURE);
 	}
 	if (argc > 2) {
@@ -201,16 +274,9 @@ int main (int argc, char ** argv) {
 	if (argc > 3) {
 		repeticiones = atoi(argv[3]);
 	}
-  if (argc > 4) {
-    tamano = atoi(argv[4]);
-  }
-  if (argc > 5) {
-    codigoATestear = argv[4];
-    if (strcmp(codigoATestear, "asm") != 0 && strcmp(codigoATestear, "c") != 0) {
-      printf("Lenguaje a testear desconocido. Las opciones son: c, asm");
-      exit(EXIT_FAILURE);
-    }
-  }
+	if (argc > 4) {
+		tamano = atoi(argv[4]);
+	}
 	int save_out = dup(1);
 	remove(archivoResultados);
 	int pFile = open(archivoResultados, O_RDWR|O_CREAT|O_APPEND, 0600);
